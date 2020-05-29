e23a2ada04c779560ce93afc2dcaefb101869eb4
kanako-sakai <knksk.526@gmail.com>
2020-05-29 19:04:15 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果：無し</p>
+        <form action="sample.php" method="POST">
+            <button type="sumbit" name="act" valule="draw">おみくじをひく！</button>
+        </form>
+    </body>
+</html>
